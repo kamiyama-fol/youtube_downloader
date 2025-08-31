9d1ec6dd57d566e111a1da7356fa7cf43f909a23
KoukiFOL <kamiyama.fol@gmail.com>
2025-08-31 17:47:44 +0900

    wsgi.pyの追加

---
+++ wsgi.py
@@ -0,0 +1,11 @@
+import sys
+# アプリケーションのルートディレクトリをPythonのパスに追加
+sys.path.insert(0, '/var/www/youtube_downloader')
+
+# app.pyからFlaskアプリケーションをインポートし、
+# mod_wsgiが期待する「application」という名前で公開します。
+from app import app as application
+
+# オプション: Apacheがリバースプロキシとして機能する場合、ProxyFixを適用
+# from werkzeug.middleware.proxy_fix import ProxyFix
+# application = ProxyFix(application)
